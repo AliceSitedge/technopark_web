ebeea7a057e7ccc24fc7fe55d62ebb2401a9e34c
Alice Sitedge <a.seledkina@mail.ru>
2019-11-04 21:28:30 +0300

    2 tasks done

---
+++ ask_alice/questions/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('question/<int:question_id>/', views.question, name='question'),
+    path('ask/', views.ask, name='ask'),
+    path('tag/<str:tag_name>/', views.tag, name='tag'),
+    path('settings/', views.settings, name='settings'),
+    path('signin/', views.signin, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('hot/', views.hot, name='hot_questions')
+]
---
+++ ask_alice/questions/views.py
@@ -0,0 +1,147 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.core.paginator import Paginator
+
+popular_tags = ['python', 'MySQL', 'django', 'mail.ru', 'technopark', 'yandex.ru']
+best_members = ['AliceSitedge', 'AntonyMo', 'LenkaDEA', 'aria_ramm', 'lavender_from_the_valley']
+
+
+def index(request):
+    template = loader.get_template('index.html')
+    questions = []
+    for i in range(1, 34):
+        questions.append({
+            'id': i,
+            'title': 'How to build a moon park?',
+            'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut '
+                    'labore et dolore magna aliqua. Nisl tincidunt eget nullam non...',
+            'answers': 3,
+            'tags': ['black-jack', 'bender']
+        })
+
+    paginator = Paginator(questions, 5)
+    page = request.GET.get('page')
+    questions_on_page = paginator.get_page(page)
+    context = {
+        'popular_tags': popular_tags,
+        'best_members': best_members,
+        'questions': questions_on_page
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def question(request, question_id):
+    template = loader.get_template('question.html')
+    question = {
+        'title': 'How to build a moon park?',
+        'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut '
+                'labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget '
+                'est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa. Commodo odio aenean '
+                'sed adipiscing diam donec adipiscing tristique. Mi eget mauris pharetra et. Non tellus orci ac '
+                'auctor augue. Elit at imperdiet dui accumsan sit. Ornare arcu dui vivamus arcu felis. Egestas '
+                'integer eget aliquet nibh praesent. In hac habitasse platea dictumst quisque sagittis purus. '
+                'Pulvinar elementum integer enim neque volutpat ac.',
+        'tags': ['black-jack', 'bender']
+    }
+
+    answers = []
+    for i in range(1, 4):
+        answers.append({
+            'text': 'Senectus et netus et malesuada. Nunc pulvinar sapien et ligula ullamcorper malesuada proin. '
+                    'Neque convallis a cras semper auctor. Libero id faucibus nisl tincidunt eget. Leo a diam '
+                    'sollicitudin tempor id. A lacus vestibulum sed arcu non odio euismod lacinia.',
+            'is_correct': True
+        })
+    context = {
+        'popular_tags': popular_tags,
+        'best_members': best_members,
+        'question_id': question_id,
+        'question': question,
+        'answers': answers
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def ask(request):
+    template = loader.get_template('ask.html')
+    context = {
+        # 'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def tag(request, tag_name):
+    template = loader.get_template('tag.html')
+    questions = []
+    for i in range(1, 34):
+        questions.append({
+            'id': i,
+            'title': 'How to build a moon park?',
+            'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut '
+                    'labore et dolore magna aliqua. Nisl tincidunt eget nullam non...',
+            'answers': 3,
+            'tags': ['black-jack', 'bender']
+        })
+
+    paginator = Paginator(questions, 5)
+    page = request.GET.get('page')
+    questions_on_page = paginator.get_page(page)
+    context = {
+        'popular_tags': popular_tags,
+        'best_members': best_members,
+        'tag_name': tag_name,
+        'questions': questions_on_page
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def settings(request):
+    template = loader.get_template('settings.html')
+    context = {
+        'popular_tags': popular_tags,
+        'best_members': best_members,
+        'nickname': 'AliceSitedge', 'login': 'alice_sitedge', 'email': 'a.seledkina@mail.ru'
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def signin(request):
+    template = loader.get_template('signin.html')
+    context = {
+        'popular_tags': popular_tags,
+        'best_members': best_members,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def signup(request):
+    template = loader.get_template('signup.html')
+    context = {
+        'popular_tags': popular_tags,
+        'best_members': best_members,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def hot(request):
+    template = loader.get_template('hot_questions.html')
+    questions = []
+    for i in range(1, 34):
+        questions.append({
+            'id': i,
+            'title': 'How to build a moon park?',
+            'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut '
+                    'labore et dolore magna aliqua. Nisl tincidunt eget nullam non...',
+            'answers': 3,
+            'tags': ['black-jack', 'bender']
+        })
+
+    paginator = Paginator(questions, 5)
+    page = request.GET.get('page')
+    questions_on_page = paginator.get_page(page)
+    context = {
+        'popular_tags': popular_tags,
+        'best_members': best_members,
+        'questions': questions_on_page
+    }
+    return HttpResponse(template.render(context, request))
