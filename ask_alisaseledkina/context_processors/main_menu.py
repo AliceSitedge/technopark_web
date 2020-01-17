77bef15c451842339c06c1df8b10dcb75f030f7a
AliceSitedge <a.seledkina@mail.ru>
2020-01-17 07:56:12 +0300

    context processor fixed

--- ask_alisaseledkina/context_processors/main_menu.py
+++ ask_alisaseledkina/context_processors/main_menu.py
@@ -2,15 +2,15 @@ from django.db.models import Count
 
 from questions.models import Profile, Tag
 
-popular_tags = Tag.objects.annotate(questions_num=Count('question')).order_by('-questions_num')[0:5]
-best_members = Profile.objects.annotate(activity=Count('question') + Count('answer')).order_by('-activity')[0:5]
-
 
 def menu(request):
     try:
         profile = Profile.objects.get(id=request.user.id)
     except Profile.DoesNotExist:
         profile = None
+
+    popular_tags = Tag.objects.annotate(questions_num=Count('question')).order_by('-questions_num')[0:5]
+    best_members = Profile.objects.annotate(activity=Count('question') + Count('answer')).order_by('-activity')[0:5]
 
     return {
         'user': request.user,
